2a8778206b5837ce212bba14e9dc2f97d7cb6c38
carlosgodspeed <carlosminibics@gmail.com>
2026-02-18 11:52:30 -0300

    Projeto Finalizado

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+  @Input()
+  gameCover:string = ""
+  @Input()
+  gameLabel:string = ""
+  @Input()
+  gameType:string = "Digital PS5"
+  @Input()
+  gamePrice:string = "R$ 299,99"
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+// repassando paramaetros
